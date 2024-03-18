4c35c74faabf488990a96f14a3b36c545e8824af
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-03-18 16:23:39 +0000

    feat: Guião 5

--- Guioes/S5/pbenc_chacha20_poly1305.py
+++ Guioes/S5/pbenc_chacha20_poly1305.py
@@ -3,6 +3,7 @@ import sys
 
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
@@ -16,72 +17,69 @@ def decrypt(salt, nonce, signature, content, password):
 
     key = kdf.derive(password.encode())
     cipher = Cipher(algorithms.AES(key[:32]), modes.CTR(nonce))
-    dec = cipher.decryptor()
+    decryptor = cipher.decryptor()
 
-    ori = dec.update(content)
+    original_content = decryptor.update(content)
 
-    hmac2 = hmac.HMAC(key[32:], hashes.SHA256())
-    hmac2.update(content)
-    hmac2.verify(signature)
+    h = hmac.HMAC(key[32:], hashes.SHA256())
+    h.update(content)
+    h.verify(signature)
 
-    return ori
+    return original_content
 
 
 def encrypt(content, password):
-    (salt, nonce) = (os.urandom(16), os.urandom(16))
+    salt = os.urandom(16)
+    nonce = os.urandom(16)
 
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
-        length=64,
+        length=32,
         salt=salt,
         iterations=480000,
     )
 
     key = kdf.derive(password.encode())
+    chacha = ChaCha20Poly1305(key)
+    encrypted = chacha.encrypt(nonce, content, None)
 
-    cipher = Cipher(algorithms.AES(key[:32]), modes.CTR(nonce))
-    enc = cipher.encryptor()
+    return salt, nonce, encrypted
 
-    cipher_text = enc.update(content)
-    hmac2 = hmac.HMAC(key[32:], hashes.SHA256())
 
-    hmac2.update(cipher_text)
-    signature = hmac2.finalize()
+def setup_key():
+    return os.urandom(32)
 
-    return salt, nonce, signature, cipher_text
 
 def main(args):
     if len(args) < 2:
-        error()
+        print("Usage:"
+              "\n\tpython3 pbenc_chacha20.py enc <fich>"
+              "\n\tpython3 pbenc_chacha20.py dec <fich>")
         return
 
-    file_name = args[1]
+    filename = args[1]
 
     password = input()
 
     if args[0] == 'enc':
-        with open(file_name, 'rb') as file_to_encrypt:
+        with open(filename, 'rb') as file_to_encrypt:
             content = file_to_encrypt.read()
             salt, nonce, signature, cipher_text = encrypt(content, password)
-        with open(file_name + '.enc', 'wb') as output_file:
+        with open(filename + '.enc', 'wb') as output_file:
             output_file.write(salt)
             output_file.write(nonce)
             output_file.write(signature)
             output_file.write(cipher_text)
-    elif args[0] == 'dec':
-        with open(file_name, 'rb') as file_to_decrypt:
-            salt, nonce, signature, content = file_to_decrypt.read(16), file_to_decrypt.read(16), file_to_decrypt.read(32), file_to_decrypt.read()
-        with open(file_name + '.dec', 'wb') as output_file:
+    if args[0] == 'dec':
+        with open(filename, 'rb') as file_to_decrypt:
+            salt = file_to_decrypt.read(16)
+            nonce = file_to_decrypt.read(16)
+            signature = file_to_decrypt.read(32)
+            content = file_to_decrypt.read()
+        with open(filename + '.dec', 'wb') as output_file:
             output_file.write(decrypt(salt, nonce, signature, content, password))
-    else:
-        error()
 
-def error():
-    print("Usage:"
-          "\n\tpython3 pbenc_chacha20.py enc <fich>"
-          "\n\tpython3 pbenc_chacha20.py dec <fich>")
-    sys.exit(1)
-    
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
